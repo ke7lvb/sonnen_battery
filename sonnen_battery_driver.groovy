aa1f9ac3034d129939f2cf4f49d480fa0b98e188
ke7lvb <67347096+ke7lvb@users.noreply.github.com>
2021-10-20 11:40:59 -0600

    Update sonnen_battery_driver.groovy
    Changed many of the 'string' attributes to 'number'
    Add an auto-refresh option for every 30 seconds.

--- sonnen_battery_driver.groovy
+++ sonnen_battery_driver.groovy
@@ -12,30 +12,30 @@ metadata {
     capability "VoltageMeasurement"
     capability "Refresh"
 
-    attribute "BackupBuffer", "string"
+    attribute "BackupBuffer", "number"
     attribute "BatteryCharging", "string"
     attribute "BatteryDischarging", "string"
-    attribute "Consumption_Avg", "string"
-    attribute "Consumption_W", "string"
-    attribute "Fac", "string"
+    attribute "Consumption_Avg", "number"
+    attribute "Consumption_W", "number"
+    attribute "Fac", "number"
     attribute "FlowConsumptionBattery", "string"
     attribute "FlowConsumptionGrid", "string"
     attribute "FlowConsumptionProduction", "string"
     attribute "FlowGridBattery", "string"
     attribute "FlowProductionBattery", "string"
     attribute "FlowProductionGrid", "string"
-    attribute "GridFeedIn_W", "string"
+    attribute "GridFeedIn_W", "number"
     attribute "IsSystemInstalled", "number"
     attribute "OperatingMode", "number"
-    attribute "Pac_total_W", "string"
-    attribute "Production_W", "string"
-    attribute "RSOC", "string"
-    attribute "RemainingCapacity_W", "string"
+    attribute "Pac_total_W", "number"
+    attribute "Production_W", "number"
+    attribute "RSOC", "number"
+    attribute "RemainingCapacity_W", "number"
     attribute "SystemStatus", "string"
     attribute "Timestamp", "string"
-    attribute "USOC", "string"
-    attribute "Uac", "string"
-    attribute "Ubat", "string"
+    attribute "USOC", "number"
+    attribute "Uac", "number"
+    attribute "Ubat", "number"
     attribute "flow_tile_large", "string"
     attribute "flow_tile_small", "string"
     attribute "connected", "string"
@@ -45,6 +45,7 @@ metadata {
 		input name: "battery_ip_address", type: "string", title: "Sonnen battery LAN IP", description: "example: 192.168.0.2", required: true
 		input("refresh_interval", "enum", title: "How often to refresh the battery data", options: [
             0:"Do NOT update",
+            30:"30 seconds",
             1:"1 Minute",
             5:"5 Minutes",
             10:"10 Minutes",
@@ -55,7 +56,7 @@ metadata {
   }
 }
 
-def version(){ return "1.0.3" }
+def version(){ return "1.0.4" }
 
 def installed(){
   if(logEnable) log.info "Driver installed"
@@ -73,7 +74,11 @@ def updated(){
   if(logEnable) log.info "Settings updated"
   if( settings.refresh_interval != "0"){
     refresh()
-    schedule("0 */${settings.refresh_interval} * ? * *", refresh)
+    if(settings.refresh_interval == "30"){
+      schedule("0/${settings.refresh_interval} * * * * ? *", refresh)
+    }else{
+      schedule("0 */${settings.refresh_interval} * ? * *", refresh)
+    }
   }
   state.version = version()
 }
@@ -89,35 +94,35 @@ def refresh() {
 				 timeout: 30
 				]) {
 		  resp -> def respData = resp.data
-		  sendEvent(name: "BackupBuffer", value: respData.BackupBuffer + "%")
+		  sendEvent(name: "BackupBuffer", value: respData.BackupBuffer)
 		  sendEvent(name: "BatteryCharging", value: respData.BatteryCharging)
 		  sendEvent(name: "BatteryDischarging", value: respData.BatteryDischarging)
-		  sendEvent(name: "Consumption_Avg", value: formatEnergy(respData.Consumption_Avg))
-		  sendEvent(name: "Consumption_W", value: formatEnergy(respData.Consumption_W))
-		  sendEvent(name: "Fac", value: respData.Fac + "Hz")
+		  sendEvent(name: "Consumption_Avg", value: (respData.Consumption_Avg))
+		  sendEvent(name: "Consumption_W", value: (respData.Consumption_W))
+		  sendEvent(name: "Fac", value: respData.Fac)
 		  sendEvent(name: "FlowConsumptionBattery", value: respData.FlowConsumptionBattery)
 		  sendEvent(name: "FlowConsumptionGrid", value: respData.FlowConsumptionGrid)
 		  sendEvent(name: "FlowConsumptionProduction", value: respData.FlowConsumptionProduction)
 		  sendEvent(name: "FlowGridBattery", value: respData.FlowGridBattery)
 		  sendEvent(name: "FlowProductionBattery", value: respData.FlowProductionBattery)
 		  sendEvent(name: "FlowProductionGrid", value: respData.FlowProductionGrid)
-		  sendEvent(name: "GridFeedIn_W", value: formatEnergy(respData.GridFeedIn_W))
+		  sendEvent(name: "GridFeedIn_W", value: (respData.GridFeedIn_W))
 		  sendEvent(name: "IsSystemInstalled", value: respData.IsSystemInstalled)
 		  sendEvent(name: "OperatingMode", value: respData.OperatingMode)
-		  sendEvent(name: "Pac_total_W", value: formatEnergy(respData.Pac_total_W))
-		  sendEvent(name: "Production_W", value: formatEnergy(respData.Production_W))
-		  sendEvent(name: "RSOC", value: respData.RSOC + "%")
-		  sendEvent(name: "RemainingCapacity_W", value: formatEnergy(respData.RemainingCapacity_W) + "h")
+		  sendEvent(name: "Pac_total_W", value: (respData.Pac_total_W))
+		  sendEvent(name: "Production_W", value: respData.Production_W)
+		  sendEvent(name: "RSOC", value: respData.RSOC)
+		  sendEvent(name: "RemainingCapacity_W", value: (respData.RemainingCapacity_W))
 		  sendEvent(name: "SystemStatus", value: respData.SystemStatus)
 		  sendEvent(name: "Timestamp", value: respData.Timestamp)
-		  sendEvent(name: "USOC", value: respData.USOC + "%")
-		  sendEvent(name: "Uac", value: respData.Uac + "V")
-		  sendEvent(name: "Ubat", value: respData.Ubat + "V")
+		  sendEvent(name: "USOC", value: respData.USOC)
+		  sendEvent(name: "Uac", value: respData.Uac)
+		  sendEvent(name: "Ubat", value: respData.Ubat)
 		  sendEvent(name: "battery", value: respData.USOC)
-		  sendEvent(name: "power", value: respData.Production_W - respData.Consumption_W + " W")
-		  sendEvent(name: "voltage", value: respData.Uac + " V")
-		  sendEvent(name: "frequency", value: respData.Fac + " Hz")
-		  sendEvent(name: "energy", value: (respData.Production_W - respData.Consumption_W) / 1000 + " kW")
+		  sendEvent(name: "power", value: respData.Production_W - respData.Consumption_W)
+		  sendEvent(name: "voltage", value: respData.Uac)
+		  sendEvent(name: "frequency", value: respData.Fac)
+		  sendEvent(name: "energy", value: (respData.Production_W - respData.Consumption_W))
 		  sendEvent(name: "powerSource", value: (respData.FlowConsumptionBattery == true) ? "battery" : "mains")
 		  sendEvent(name: "connected", value: "true")
 		  if(logEnable) log.info respData
@@ -142,11 +147,11 @@ def updateTiles() {
   def current_production = device.currentValue("Production_W")
 
   def flow_tile_large = "<div><table style='margin: auto'>"
-  flow_tile_large += "<tr><td></td><td></td><td>" + device.currentValue("Production_W") + "</td><td></td><td></td></tr>"
+  flow_tile_large += "<tr><td></td><td></td><td>" + formatEnergy(device.currentValue("Production_W")) + "</td><td></td><td></td></tr>"
   flow_tile_large += "<tr><td></td><td>" + (device.currentValue("FlowConsumptionProduction") == "true" ? "<img src=\"https://img.icons8.com/material-sharp/48/26e07f/left-down2.png\"/>" : "") + "</td><td><img src=\"https://img.icons8.com/material-outlined/48/4a90e2/sun--v1.png\"/></td><td>" + (device.currentValue("FlowProductionBattery") == "true" ? "<img src=\"https://img.icons8.com/material-outlined/48/26e07f/right-down2.png\"/>" : "") + "</td><td></td></tr>"
-  flow_tile_large += "<tr><td>" + device.currentValue("Consumption_W") + "</td><td><img src='https://img.icons8.com/material-outlined/48/4a90e2/cottage.png'/></td><td>" + (device.currentValue("FlowProductionGrid") == "true" ? "<img src=\"https://img.icons8.com/material-rounded/48/26e07f/long-arrow-down.png\"/>" : "") + (device.currentValue("FlowConsumptionBattery") == "true" ? "<img src=\"https://img.icons8.com/material-rounded/48/26e07f/long-arrow-left.png\"/>" : "") + "</td><td><img src='https://img.icons8.com/ios-glyphs/48/4a90e2/battery--v1.png'/></td><td>" + device.currentValue("Pac_total_W") + "</td></tr>"
+  flow_tile_large += "<tr><td>" + formatEnergy(device.currentValue("Consumption_W")) + "</td><td><img src='https://img.icons8.com/material-outlined/48/4a90e2/cottage.png'/></td><td>" + (device.currentValue("FlowProductionGrid") == "true" ? "<img src=\"https://img.icons8.com/material-rounded/48/26e07f/long-arrow-down.png\"/>" : "") + (device.currentValue("FlowConsumptionBattery") == "true" ? "<img src=\"https://img.icons8.com/material-rounded/48/26e07f/long-arrow-left.png\"/>" : "") + "</td><td><img src='https://img.icons8.com/ios-glyphs/48/4a90e2/battery--v1.png'/></td><td>" + formatEnergy(device.currentValue("Pac_total_W")) + "</td></tr>"
   flow_tile_large += "<tr><td></td><td>" + (device.currentValue("FlowConsumptionGrid") == "true" ? "<img src=\"https://img.icons8.com/material-outlined/48/fa314a/left-up2.png\"/>" : "" ) + "</td><td><img src=\"https://img.icons8.com/ios/48/4a90e2/transmission-tower.png\"/></td><td>" + ((device.currentValue("FlowGridBattery") == "true" && device.currentValue("BatteryCharging") == "true") ? "<img src=\"https://img.icons8.com/material-outlined/48/fa314a/right-up2.png\"/>" : "") + ((device.currentValue("FlowGridBattery") == "true" && device.currentValue("BatteryDischarging") == "true") ? "<img src=\"https://img.icons8.com/material-outlined/48/26e07f/down-left.png\"/>" : "") + "</td><td></td></tr>"
-  flow_tile_large += "<tr><td></td><td></td><td>" + device.currentValue("GridFeedIn_W") + "</td><td></td><td></td></tr>"
+  flow_tile_large += "<tr><td></td><td></td><td>" + formatEnergy(device.currentValue("GridFeedIn_W")) + "</td><td></td><td></td></tr>"
   flow_tile_large += "</table></div>"
 
   sendEvent(name: "flow_tile_large", value: flow_tile_large)
